21a4b5e731d9113145a5ced69353ed4c78234255
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-11 23:13:09 +0300

    1.4 Получение погоды по API

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Alert} from 'react-native';
 import * as Location from 'expo-location';
 import Loading from './Loading';
+import axios from 'axios';
 
+const API_KEY = '2002e58b1b63a441b83a6b24a53cf29a';
 
 export default class extends React.Component {
 
@@ -11,12 +13,17 @@ export default class extends React.Component {
     isLoading: true
   }
 
+  getWeather = async (latitude, longitude) => {
+    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  }
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({isLoading: false});
-      // TODO: Сделать запрос к API
     } catch (error) {
       Alert.alert('Не могу определить местоположение', "Очень грустно :(");
     }
