b07dcbae43b1ac7d82f54381963e1471a15b48bf
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-11 20:58:18 +0300

    1.1 Экран загрузки

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-      </View>
-      <View style={styles.blueView}>
-      </View>
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: 'yellow'
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: 'blue'
-  }
-});
---
+++ Loading.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+    return (<View style={styles.container}>
+        <Text style={styles.text}>Получение погоды...</Text>
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:"flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#FDF6AA",
+    },
+    text: {
+        color: "#2c2c2c",
+        fontSize: 30
+    }
+})
