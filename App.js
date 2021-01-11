4f33b77463eafe887d3179dc95f93e05c8683fd9
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-11 21:47:23 +0300

    1.2 Получение геопозиции

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+
+export default class extends React.Component {
+
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render () {
+    return (
+      <Loading />
+    );
+  }
 }
